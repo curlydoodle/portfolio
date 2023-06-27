d87a16d318db4092ff4a8f3ef0cd4bfba8e3bbc9
MustyDev <www.mustafakerem22@gmail.com>
2023-06-27 20:44:27 +0300

    Small fix

--- src/lib/about-me.svelte
+++ src/lib/about-me.svelte
@@ -5,7 +5,7 @@
                 <hr class="w-64 h-px my-6 bg-gray-200 border-none">
                 <span class="text-lg md:text-xl absolute px-6 -translate-x-1/2 bg-background left-1/2">About Me</span>
             </div>
-            <p class="font-normal text-xs md:text-sm">I am a 16 year old high school student and programming freak from Kocaeli, Turkiye. I like to program apps and improve my skills.</p>
+            <p class="font-normal text-xs md:text-sm text-center">I am a 16 year old high school student and programming freak from Kocaeli, Turkiye. I like to program apps and improve my skills.</p>
         </div>
     </div>
 </div>
