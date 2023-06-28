1eb0e7d94d1e9338cdc0a5cac7de50195d80f2d8
MustyDev <www.mustafakerem22@gmail.com>
2023-06-28 16:45:50 +0300

    Updated + Added Sounds

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -1,3 +1,9 @@
-<button class="btn bg-theme rounded-xl px-4 py-1 my-1 text-theme-text text-sm ring-theme ring-offset-2 hover:ring-2">
+<script lang="ts">
+    import { useSound } from "svelte-sound";
+    import click_mp4 from "$lib/assets/click.mp4";
+    const click_sound = useSound(click_mp4, ["click"])
+</script>
+
+<button use:click_sound class="btn bg-theme rounded-xl px-4 py-1 my-1 text-theme-text text-sm ring-theme ring-offset-2 hover:ring-2">
     <slot name="text"></slot>
 </button>
