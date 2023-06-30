cc654ee0e4a5c872cf0595adc9a86d2acda1a5e7
MustyDev <www.mustafakerem22@gmail.com>
2023-06-30 20:24:07 +0300

    Update

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -2,8 +2,10 @@
     import { useSound } from "svelte-sound";
     import click_mp4 from "$lib/assets/click.mp4";
     const click_sound = useSound(click_mp4, ["click"])
+
+    export let text = "Button"
 </script>
 
 <button use:click_sound class="btn bg-theme rounded-xl px-4 py-1 my-1 text-theme-text text-sm ring-theme ring-offset-2 ring-offset-background hover:ring-2">
-    <slot name="text"></slot>
+    {text}
 </button>
