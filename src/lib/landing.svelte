27b40df2f7b93ff655b5a2ef72341b3e84396ad0
MustyDev <www.mustafakerem22@gmail.com>
2023-06-30 23:43:21 +0300

    Fixed Mistake

--- src/lib/landing.svelte
+++ src/lib/landing.svelte
@@ -5,7 +5,7 @@
 <div class="text-center position-relative z-50">
     <div class="text-xl">Mustafa Akın</div>
     <div class="flex flex-row justify-center text-sm">Front-End & Game Developer</div>
-    <a href="https://discord.com/users/501804360079245312" target="_blank">
+    <form action="https://discord.com/users/501804360079245312" target="_blank" method="get">
         <Button text="Contact"/>
-    </a>
+    </form>
 </div>
