220ddd508177a1181e53ca44b43f2e674fac6f3f
MustyDev <www.mustafakerem22@gmail.com>
2023-07-06 00:04:16 +0300

    Fix

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import { useSound } from "svelte-sound";
+    import { sound } from "svelte-sound";
     import click_mp4 from "$lib/assets/click.mp4";
-    const click_sound = useSound(click_mp4, ["click"])
 </script>
 
-<button on:click on:keypress use:click_sound class="btn text-center bg-theme rounded-xl px-3 py-1 my-2 text-xs">
+<button use:sound={{src: click_mp4, events: ["click"]}} on:click on:keypress class="btn text-center bg-theme hover:bg-gray-800 transition ease-out rounded-xl px-3 py-1 my-2 text-xs">
     <slot name="text"></slot>
 </button>
